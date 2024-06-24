9142d4407b84d14ce40269e6812d45ea4ede9f41
perlancar (on netbook-dell-xps13) <perlancar@gmail.com>
2024-06-24 14:20:28 +0700

    Initial commit

---
+++ lib/App/OCRUtils.pm
@@ -0,0 +1,26 @@
+package App::OCRUtils;
+
+use 5.010001;
+use strict;
+use warnings;
+
+# AUTHORITY
+# DATE
+# DIST
+# VERSION
+
+our %SPEC;
+
+1;
+#ABSTRACT: Utilities related to OCR
+
+=head1 DESCRIPTION
+
+This distributions provides the following command-line utilities:
+
+# INSERT_EXECS_LIST
+
+
+=head1 SEE ALSO
+
+=cut
